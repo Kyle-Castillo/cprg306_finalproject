680f0b776515329fcb6471673aa4ef17d0c10a91
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2023-12-03 16:37:31 -0700

    more stuff

---
+++ app/proile.js
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main>
+      <div className='top-bar'>
+        <div className='logo-text'>
+          <h1 className='logo-text'>Bookworm</h1>
+        </div>
+        <div className='top-menu'>
+          <button className='quick-menu-button'>Home</button>
+          <button className='quick-menu-button'>Books</button>
+          <button className='quick-menu-button'>Search</button>
+        </div>
+      </div>
+      <div className='main-body'>
+        <div className='user-profile'>
+            <p className='user-name'>Name: </p>
+            <p className='user-books-read'>Books read: </p>
+            <p className='user-books-list'>Books in reading list: </p>
+            <p className='user-books-current'>Currently reading: </p>
+        </div>
+        <div className='user-data'>
+          <div className='books-read-list'>
+            <h1 className='books-read'>You've finished reading:</h1>
+          </div>
+          <div className='books-reading-list'>
+            <h1 className='future-books'>You plan to read:</h1>
+          </div>
+          <div className=''>
+
+          </div>
+
+        </div>
+      </div>
+    </main>
+  )
+}
