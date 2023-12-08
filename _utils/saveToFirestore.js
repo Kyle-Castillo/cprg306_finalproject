4e53ec5e02c6130ef8a769b263291bcae4affb5c
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2023-12-07 17:49:27 -0700

    Update saveToFirestore.js

--- _utils/saveToFirestore.js
+++ _utils/saveToFirestore.js
@@ -1,34 +1,37 @@
-import { doc, setDoc, remove, getDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 import { db } from './firebase';
 
-
 const saveBookToFirestore = async (book, status, add = true) => {
-    const userId = '';
+  const auth = getAuth();
+  const user = auth.currentUser;
 
-    if (!userId) {
-        console.error('User ID not available.');
-        return;
-    }
+  if (!user) {
+    console.error('User not authenticated.');
+    return;
+  }
 
-    const userDocRef = doc(db, 'users', userId);
-    const userDoc = await getDoc(userDocRef);
+  const userId = user.uid;
 
-    if (userDoc.exists()) {
-        const savedBooks = userDoc.data().savedBooks || [];
+  const userDocRef = doc(db, 'users', userId);
+  const userDoc = await getDoc(userDocRef);
 
-        if (add) {
-            savedBooks.push({ ...book, status});
-        } else {
-            const indexToRemove = savedBooks.findIndex((savedBook) => savedBook.key === book.key);
-            if (indexToRemove !== 1) {
-                savedBooks.splice(indexToRemove, 1);
-            }
-        }
+  if (userDoc.exists()) {
+    const savedBooks = userDoc.data().savedBooks || [];
 
-        await set(userDocRef, { savedBooks }, { merge: true});
+    if (add) {
+      savedBooks.push({ ...book, status });
     } else {
-        console.error('User Document not found.');
+      const indexToRemove = savedBooks.findIndex((savedBook) => savedBook.key === book.key);
+      if (indexToRemove !== -1) {
+        savedBooks.splice(indexToRemove, 1);
+      }
     }
+
+    await setDoc(userDocRef, { savedBooks }, { merge: true });
+  } else {
+    console.error('User Document not found.');
+  }
 };
 
 export default saveBookToFirestore;
