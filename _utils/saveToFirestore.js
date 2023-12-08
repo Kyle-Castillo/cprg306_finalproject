0f61f7d76434498a118b4228aabcaf7637846412
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2023-12-07 23:53:58 -0700

    Update saveToFirestore.js

--- _utils/saveToFirestore.js
+++ _utils/saveToFirestore.js
@@ -17,11 +17,31 @@ const saveBookToFirestore = async (book, status, add = true, setSavedBooks) => {
 
     if (userDoc.exists()) {
       const savedBooks = userDoc.data().savedBooks || [];
+      let updatedStatus;
+
+      switch (status) {
+        case 'Plan to read':
+          updatedStatus = 'planToRead';
+          break;
+        case 'Favorite books':
+          updatedStatus = 'favoriteBooks';
+          break;
+        case 'Already Read':
+          updatedStatus = 'booksRead';
+          break;
+        case 'Currently Reading':
+          updatedStatus = 'currentlyReading';
+          break;
+        default:
+          updatedStatus = status;
+      }
 
       if (add) {
-        savedBooks.push({ ...book, status });
+        savedBooks.push({ ...book, status: updatedStatus });
       } else {
-        const indexToRemove = savedBooks.findIndex((savedBook) => savedBook.key === book.key);
+        const indexToRemove = savedBooks.findIndex(
+          (savedBook) => savedBook.key === book.key
+        );
         if (indexToRemove !== -1) {
           savedBooks.splice(indexToRemove, 1);
         }
