24fd8139a32341b6f433398a2cd59c1efd5b6388
Kyle-Castillo <kyle.castillo@edu.sait.ca>
2023-12-07 23:30:58 -0700

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import fetchBooks from './api/openLibrary';
 import saveBookToFirestore from '@/_utils/saveToFirestore';
 import importBooksToFirestore from '@/_utils/importBooksToFirestore';
